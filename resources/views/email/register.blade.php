8e0d6cd06cb138c43f9b94607f6d8b24af952028
muha9mmad81 <muha9mmad81@gmail.com>
2024-09-27 23:14:46 +0500

    changes

--- resources/views/email/register.blade.php
+++ resources/views/email/register.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
     <p>Hello <b>{{ $user->name ?? '' }}</b>,</p>
     <p>Please Activate your account now by clicking below link.</p>
-    <p><a href="https://buzzinguniverse.iqspark.org/activation-account/activation-accoun?token={{ $token }}">Activate
+    <p><a href="https://buzzinguniverse.iqspark.org/activation-account/activation-account.html?token={{ $token }}">Activate
             your account</a></p>
     <p>For any further assistance or questions, feel free to reach out to our support team. We are here to assist you.</p>
     <p>Thank you for choosing {{ config('app.name') }}. We value your security and appreciate your cooperation.</p>
